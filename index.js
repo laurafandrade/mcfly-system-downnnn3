535952ff5ee5e5e6ae88ff3b7ef056a15ffa4832
laurafandrade <laurafandradey@gmail.com>
2025-05-31 18:08:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
-import makeWASocket from '@whiskeysockets/baileys'
-import { Boom } from '@hapi/boom'
-import fs from 'fs'
+import makeWASocket from "@whiskeysockets/baileys";
 
-const authFile = './auth_info_baileys.json'
-
-async function start() {
-  let authInfo = undefined
-
-  if (fs.existsSync(authFile)) {
-    authInfo = JSON.parse(fs.readFileSync(authFile, 'utf-8'))
-  }
-
-  const sock = makeWASocket({
-    auth: authInfo
-  })
+async function startSock() {
+  const sock = makeWASocket();
 
   sock.ev.on('connection.update', (update) => {
-    const { connection, lastDisconnect, qr } = update
-
-    if (qr) {
-      console.log('Escaneie o QR Code abaixo:')
-      console.log(qr)
+    const { connection, qr } = update;
+    if(qr) {
+      console.log('Escaneie o QR code abaixo:\n', qr);
     }
-
-    if (connection === 'close') {
-      const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== Boom.statusCodes['loggedOut']
-
-      console.log('Conexão fechada, motivo:', lastDisconnect.error)
-      if (shouldReconnect) {
-        start()
-      } else {
-        console.log('Usuário desconectou, reinicie o bot para reconectar')
-      }
+    if(connection === 'close') {
+      console.log('Conexão fechada, tentando reconectar...');
+      startSock();
     }
-
-    if (connection === 'open') {
-      console.log('Conectado ao WhatsApp!')
+    if(connection === 'open') {
+      console.log('Conectado ao WhatsApp!');
     }
-  })
-
-  sock.ev.on('creds.update', () => {
-    const auth = sock.authState
-    fs.writeFileSync(authFile, JSON.stringify(auth, null, 2))
-  })
+  });
 }
 
-start()
+startSock();
